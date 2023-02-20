4e5238bb72700b9d82fbc8f4f4a5c716ec71609b
Param Pal Singh <singhparampal97@gmail.com>
2023-02-20 14:45:42 +0100

    Create Employee Route + Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Container } from "@chakra-ui/react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
+import Employee from "./components/Employee";
 
 function App() {
   return (
@@ -8,6 +9,11 @@ function App() {
       <Header />
       <Container pt="6" maxW="container.md">
         {/* add non-header content here */}
+        <Router>
+          <Routes>
+            <Route path="/employees/:id" element={<Employee />}></Route>
+          </Routes>
+        </Router>
       </Container>
     </>
   );
